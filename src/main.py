5c936a042357464b47f2602c13e4dea7f6fe9149
hfrangon <109893156+hfrangon@users.noreply.github.com>
2024-09-21 22:02:32 +0800

    ##训练过程添加余弦退火

--- src/main.py
+++ src/main.py
@@ -80,7 +80,7 @@ def main(opt):
         for k, v in log_dict_train.items():
             logger.scalar_summary('train_{}'.format(k), v, epoch)
             logger.write('{} {:8f} | '.format(k, v))
-            logger.write('|lr :{:8f}'.format(scheduler.get_last_lr()))
+            #logger.write('|lr :{:8f}'.format(scheduler.get_last_lr()))
         if opt.val_intervals > 0 and epoch % opt.val_intervals == 0:
             save_model(os.path.join(opt.save_dir, 'model_{}.pth'.format(mark)),
                        epoch, model, optimizer)
